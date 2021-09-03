6f9bc1bb76d732e411c57eaa506906e7bd82ffb5
JPPalacios <56488109+JPPalacios@users.noreply.github.com>
2021-09-03 13:11:01 -0400

    update w format
    9/3 1:10pm

--- main.c
+++ main.c
@@ -4,24 +4,23 @@ void main(void)
 {
     WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;     // stop watchdog timer
 
-    //button 1 init:
-    P5-> SEL0 &= ~BIT1; //GPIO
-    P5-> SEL1 &= ~BIT1; //GPIO
-    P5-> DIR &= ~BIT1;  //input
-    P5-> REN |= BIT1;   //resistor enable
-    P5-> OUT |= BIT1;   //pull-up
+    //  Port 5.1 - Button 1
+    P5->SEL0 &= ~BIT1;  // GPIO
+    P5->SEL1 &= ~BIT1;  // GPIO
+    P5->DIR  &= ~BIT1;  // Direction Register -> Input
+    P5->REN  |=  BIT1;  // Resistor Enable Register
+    P5->OUT  |=  BIT1;  // Data Output Register -> Pull-up
+    P5->IES  |=  BIT1;  // Interrupt Edge Select Register
+    P5->IE   |=  BIT1;  // Interrupt Enable Register
 
-    P5-> IES |= BIT1;   // Port X Interrupt Edge Select Register
-    P5-> IE  |= BIT1;   // Port X Interrupt Enable Register
-
-    //button 2 init:
-    P5-> SEL0 &= ~BIT2; //GPIO
-    P5-> SEL1 &= ~BIT2; //GPIO
-    P5-> DIR &= ~BIT2; //input
-    P5-> REN |= BIT2; //resistor enable
-    P5-> OUT |= BIT2; //pull-up
-    P5-> IES |= BIT2; // Interrupt Service Routine
-    P5-> IE  |= BIT2;
+    //  Port 5.2 - Button 1
+    P5->SEL0 &= ~BIT2;  // GPIO
+    P5->SEL1 &= ~BIT2;  // GPIO
+    P5->DIR  &= ~BIT3;  // Direction Register -> Input
+    P5->REN  |=  BIT2;  // Resistor Enable Register
+    P5->OUT  |=  BIT2;  // Data Output Register -> Pull-up
+    P5->IES  |=  BIT2;  // Interrupt Edge Select Register
+    P5->IE   |=  BIT2;  // Interrupt Enable Register
 
     //red, green, blue (respectively)LED init:
     P2-> SEL1 &= ~(BIT4 | BIT5 | BIT6); //GPIO
